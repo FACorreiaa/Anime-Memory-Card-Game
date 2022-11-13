bd68fa2d1b5869ee755685ea8ca88d11d71c1963
FACorreiaa <fernandocorreia316@gmail.com>
2022-11-13 14:52:21 +0100

    update routes

--- src/App.tsx
+++ src/App.tsx
@@ -11,7 +11,7 @@ function App() {
 
 			<BrowserRouter>
 				<Routes>
-					<Route path="/" element={<GamePage />} />
+					<Route path="/" element={<MainPage />} />
 					<Route path="/game" element={<GamePage />} />
 				</Routes>
 			</BrowserRouter>
