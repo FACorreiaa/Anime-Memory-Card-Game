ec417fdce09de3dabed549689f92b47ff07f93af
fc <fc@gsmk.de>
2023-06-05 15:17:18 +0100

    change styles

--- src/pages/GamePage.tsx
+++ src/pages/GamePage.tsx
@@ -3,7 +3,6 @@ import './GamePage.css';
 import Board from '../components/Board';
 import Card, { CardObjectType } from '../components/Card';
 import PlayerTurns from '../components/PlayerTurns';
-import Header from '../components/Header';
 import Button from '../components/Button';
 const cardImages = [
 	{ src: '/img/hiruma.jpg', matched: false },
@@ -153,6 +152,13 @@ function GamePage() {
 
 	return (
 		<div className="container">
+			<div className="z-10 items-center justify-between w-full max-w-5xl font-mono text-sm lg:flex">
+				<p className="fixed top-0 left-0 flex justify-center w-full pt-8 pb-6 border-b border-gray-300 bg-gradient-to-b from-zinc-200 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
+					Memory&nbsp;
+					<code className="font-mono font-bold">Card&nbsp;</code>
+					Game
+				</p>
+			</div>
 			<div>
 				<Button onClick={onHandleStartButton} className="button">
 					{renderButtonMessage}
