aac5c8da46d4ef8772aeb84ccfe2e72cdb4adcde
fc <fc@gsmk.de>
2023-06-05 14:24:29 +0100

    change settings

--- src/App.tsx
+++ src/App.tsx
@@ -2,18 +2,20 @@ import './App.css';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import GamePage from './pages/GamePage';
 import Header from './components/Header';
-
+import { Suspense } from 'react';
 function App() {
 	return (
-		<div className="App">
-			<Header title="Memorization card game!" />
+		<Suspense fallback={<div>Loading</div>}>
+			<div className="App">
+				<Header title="Memorization card game!" />
 
-			<BrowserRouter>
-				<Routes>
-					<Route path="/" element={<GamePage />} />
-				</Routes>
-			</BrowserRouter>
-		</div>
+				<BrowserRouter>
+					<Routes>
+						<Route path="/" element={<GamePage />} />
+					</Routes>
+				</BrowserRouter>
+			</div>
+		</Suspense>
 	);
 }
 
--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,11 +1,10 @@
-import React from 'react';
 import './Header.css';
 type HeaderProps = {
 	title: string;
 };
 function Header({ title }: HeaderProps) {
 	return (
-		<div className="header-container">
+		<div className="bg-gradient-to-b from-zinc-500 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit">
 			<h1 className="title">{title}</h1>
 		</div>
 	);
